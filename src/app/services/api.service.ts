7d29cb479b0598590a3022c86d8f685546d3fd96
phev8 <phev8@users.noreply.github.com>
2019-10-10 17:14:15 +0200

    replace angular project with react

--- src/app/services/api.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-
-@Injectable()
-export class ApiService {
-    // TODO: this service is an abstraction layer to the http api of the backend services
-}
