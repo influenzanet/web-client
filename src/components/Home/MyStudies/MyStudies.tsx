051e284be5ae56eaf709fa337f29912b43ae1889
phev8 <phev8@users.noreply.github.com>
2019-11-17 21:16:00 +0100

    remove obsolete survey engine code

--- src/components/Home/MyStudies/MyStudies.tsx
+++ src/components/Home/MyStudies/MyStudies.tsx
@@ -3,28 +3,11 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { SET_PAGE_TITLE } from '../../../store/actions/actionTypes';
 
-import { SurveyEngineCore, testSurvey } from '../../../survey-engine/engine';
-import {} from '../../../survey-engine/dataTypes';
-
 const MyStudies: React.FC = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
         // const survey = renderSurvey({ surveyDef: testSurvey});
-        const context = {
-            mode: 'test',
-        }
-        const sEngine = new SurveyEngineCore(testSurvey, 'me', 'someone else', context);
-
-        /*if (survey.surveyState) {
-            printRenderedSurvey(survey.surveyState);
-        }*/
-        console.log(sEngine.responses);
-        sEngine.printRenderedSurvey();
-
-        sEngine.setResponse('qg3', 'q2', 1);
-        sEngine.printRenderedSurvey();
-
         dispatch({ type: SET_PAGE_TITLE, title: 'My Studies'});
     });
 
