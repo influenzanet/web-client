2b3fa6c80bd6c53a58b70a9240f1c3eee0dea2ff
petterol <o_petter14@cs.uni-kl.de>
2020-01-23 13:50:23 +0100

    started InfluenzanetQuestionnaires with Question 4

--- src/test-surveys/test-survey-1.ts
+++ src/test-surveys/test-survey-1.ts
@@ -1,14 +1,143 @@
-import { QuestionGroup, Question } from 'survey-engine/lib/data_types';
+import { SurveyGroupItem, SurveySingleItem } from 'survey-engine/lib/data_types';
 
-const q1: Question = {
-
-}
-
-
-export const testSurvey1: QuestionGroup = {
+export const QG_0: SurveyGroupItem = {
+  key: 'QG_0',
+  version: 1,
   items: [
-    q1,
-  ]
-};
-
-// JSON.stringify(testSurvey1, undefined, 2);
+    {
+      key: 'QG_4',
+      version: 1,
+      items: [
+        {
+          key: 'QG_4_Q_4',
+          version: 1,
+          validations: [],
+          type: "basic.input.single-choice",
+          components: [
+            {
+              role: 'title', 
+              content: [
+                { code: 'en', parts: ['What is your main activity?'] },
+                { code: 'de', parts: [''] }
+              ]
+            },
+            {
+              key: 'RG_1',
+              role: 'responseGroup', 
+              items: [
+                {
+                  key: 'RG_1_R_1',
+                  role: 'responseOption',
+                  content: [
+                    {
+                      code: 'en',
+                      parts: ['Paid employment, full-time']
+                    },
+                    {
+                      code: 'de',
+                      parts: ['']
+                    }
+                  ]
+                },
+                {
+                  key: 'RG_1_R_2',
+                  role: 'responseOption',
+                  content: [
+                    {
+                      code: 'en',
+                      parts: ['Paid employment, part-time']
+                    },
+                    {
+                      code: 'de',
+                      parts: ['']
+                    }
+                  ]
+                },
+              ]
+            },
+          ]
+        },
+        {
+          key: 'QG_4_Q_4b',
+          condition: {
+            name: 'or',
+            data: [
+              {
+                dtype: "test",
+                exp: 
+                {
+                  name: 'gotYes',
+                  data: [
+                    {
+                      dtype: 'test',
+                      str: 'QG_4_Q_4_RG_1_R_1',
+                    }
+                  ]
+                },
+              },
+              {
+                dtype: "test",
+                exp: 
+                {
+                  name: 'gotYes',
+                  data: [
+                    {
+                      dtype: 'test',
+                      str: 'QG_4_Q_4_RG_1_R_2',
+                    }
+                  ]
+                },
+              },
+            ]
+          },
+          version: 1,
+          validations: [],
+          type: "basic.input.single-choice",
+          components: [
+            {
+              role: 'title', 
+              content: [
+                { code: 'en', parts: ['What is the first part of your school/college/workplace postal code (where you spend the majority of your working/studying time)?'] },
+                { code: 'de', parts: [''] }
+              ]
+            },
+            {
+              key: 'RG_1',
+              role: 'responseGroup', 
+              items: [
+                {
+                  key: 'RG_1_R_1',
+                  role: 'responseOption',
+                  content: [
+                    {
+                      code: 'en',
+                      parts: ['Paid employment, full-time']
+                    },
+                    {
+                      code: 'de',
+                      parts: ['']
+                    }
+                  ]
+                },
+                {
+                  key: 'RG_1_R_2',
+                  role: 'responseOption',
+                  content: [
+                    {
+                      code: 'en',
+                      parts: ['Paid employment, part-time']
+                    },
+                    {
+                      code: 'de',
+                      parts: ['']
+                    }
+                  ]
+                },
+              ]
+            },
+          ]
+        },
+      ],
+    }
+  ],
+}
