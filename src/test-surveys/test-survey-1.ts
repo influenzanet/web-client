848fb8db91f2a44a26e5cde96ff0594a70cd1c3c
petterol <o_petter14@cs.uni-kl.de>
2020-01-23 14:40:35 +0100

    implement 4b and 4c

--- src/test-surveys/test-survey-1.ts
+++ src/test-surveys/test-survey-1.ts
@@ -107,11 +107,105 @@ export const QG_0: SurveyGroupItem = {
               items: [
                 {
                   key: 'RG_1_R_1',
+                  role: 'responseInput',
+                  content: [
+                    {
+                      code: 'en',
+                      parts: ['XXXX']
+                    },
+                    {
+                      code: 'de',
+                      parts: ['XXXX']
+                    }
+                  ]
+                },
+                {
+                  key: 'RG_1_R_2',
                   role: 'responseOption',
                   content: [
                     {
                       code: 'en',
-                      parts: ['Paid employment, full-time']
+                      parts: ['I don’t know/can’t remember']
+                    },
+                    {
+                      code: 'de',
+                      parts: ['']
+                    }
+                  ]
+                },
+                {
+                  key: 'RG_1_R_3',
+                  role: 'responseOption',
+                  content: [
+                    {
+                      code: 'en',
+                      parts: ['Not applicable (e.g. don’t have a fixed workplace)']
+                    },
+                    {
+                      code: 'de',
+                      parts: ['']
+                    }
+                  ]
+                },
+              ]
+            },
+          ]
+        },
+        {
+          key: 'QG_4_Q_4c',
+          condition: {
+            name: 'or',
+            data: [
+              {
+                dtype: "test",
+                exp: 
+                {
+                  name: 'gotYes',
+                  data: [
+                    {
+                      dtype: 'test',
+                      str: 'QG_4_Q_4_RG_1_R_1',
+                    }
+                  ]
+                },
+              },
+              {
+                dtype: "test",
+                exp: 
+                {
+                  name: 'gotYes',
+                  data: [
+                    {
+                      dtype: 'test',
+                      str: 'QG_4_Q_4_RG_1_R_2',
+                    }
+                  ]
+                },
+              },
+            ]
+          },
+          version: 1,
+          validations: [],
+          type: "basic.input.single-choice",
+          components: [
+            {
+              role: 'title', 
+              content: [
+                { code: 'en', parts: ['Which of the following descriptions most closely matches with your main occupation?'] },
+                { code: 'de', parts: [''] }
+              ]
+            },
+            {
+              key: 'RG_1',
+              role: 'responseGroup', 
+              items: [
+                {
+                  key: 'RG_1_R_1',
+                  role: 'responseOption',
+                  content: [
+                    {
+                      code: 'en',
+                      parts: ['Professional (e.g. manager, doctor, teacher, nurse, engineer)']
                     },
                     {
                       code: 'de',
@@ -125,11 +219,34 @@ export const QG_0: SurveyGroupItem = {
                   content: [
                     {
                       code: 'en',
-                      parts: ['Paid employment, part-time']
+                      parts: ['Other']
                     },
                     {
                       code: 'de',
                       parts: ['']
+                    }
+                  ]
+                },
+                {
+                  key: 'RG_1_R_3',
+                  role: 'responseInput',
+                  displayCondition: {
+                    name: 'checked',
+                    data: [
+                      {
+                        dtype: 'test',
+                        str: 'QG_4_Q_4C_RG_1_R_2',
+                      },
+                    ]
+                  },
+                  content: [
+                    {
+                      code: 'en',
+                      parts: ['XXXX']
+                    },
+                    {
+                      code: 'de',
+                      parts: ['XXXX']
                     }
                   ]
                 },
