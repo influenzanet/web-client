321f753310112bd0cf1632a7c2e7964f48abb480
petterol <o_petter14@cs.uni-kl.de>
2020-01-25 21:37:06 +0100

    updated QG4 to model v0.4.0

--- src/test-surveys/test-survey-1.ts
+++ src/test-surveys/test-survey-1.ts
@@ -1,260 +1,360 @@
 import { SurveyGroupItem, SurveySingleItem } from 'survey-engine/lib/data_types';
 
-export const QG_0: SurveyGroupItem = {
-  key: 'QG_0',
+const Q4: SurveySingleItem = {
+  key: 'QG4.Q4',
+  version: 1,
+  validations: [],
+  type: "basic.input.single-choice",
+  components: [
+    {
+      role: 'title',
+      content: [
+        {
+          code: 'en',
+          parts: [
+            {
+              str: 'What is your main activity?'
+            },
+          ]
+        },
+        {
+          code: 'de',
+          parts: [
+            {
+              str: 'XX'
+            },
+          ]
+        },
+      ]
+    },
+    {
+      key: 'RG1',
+      role: 'responseGroup',
+      items: [
+        {
+          key: 'RG1.R1',
+          role: 'responseOption',
+          content: [
+            {
+              code: 'en',
+              parts: [
+                {
+                  str: 'Paid employment, full-time'
+                },
+              ]
+            },
+            {
+              code: 'de',
+              parts: [
+                {
+                  str: 'XX'
+                },
+              ]
+            },
+          ]
+        },
+        {
+          key: 'RG1.R2',
+          role: 'responseOption',
+          content: [
+            {
+              code: 'en',
+              parts: [
+                {
+                  str: 'Paid employment, part-time?'
+                },
+              ]
+            },
+            {
+              code: 'de',
+              parts: [
+                {
+                  str: 'XX'
+                },
+              ]
+            },
+          ]
+        },
+      ]
+    },
+  ]
+}
+
+const Q4b: SurveySingleItem = {
+  key: 'QG4.Q4b',
+  condition: {
+    name: 'or',
+    data: [
+      {
+        dtype: 'exp',
+        exp: {
+          name: 'isDefined',
+          data: [
+            {
+              dtype: 'exp',
+              exp: {
+                name: 'getResponseItem',
+                data: [
+                  {
+                    str: 'QG4.Q4'
+                  },
+                  {
+                    str: 'RG1.R1'
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      },
+      {
+        dtype: 'exp',
+        exp: {
+          name: 'isDefined',
+          data: [
+            {
+              dtype: 'exp',
+              exp: {
+                name: 'getResponseItem',
+                data: [
+                  {
+                    str: 'QG4.Q4'
+                  },
+                  {
+                    str: 'RG1.R2'
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      }
+    ]
+  },
+  version: 1,
+  validations: [],
+  type: "basic.input.single-choice",
+  components: [
+    {
+      role: 'title',
+      content: [
+        {
+          code: 'en',
+          parts: [
+            {
+              str: 'What is the first part of your school/college/workplace postal code (where you spend the majority of your working/studying time)?'
+            },
+          ]
+        },
+        {
+          code: 'de',
+          parts: [
+            {
+              str: 'XX'
+            },
+          ]
+        },
+      ]
+    },
+    {
+      key: 'RG1',
+      role: 'responseGroup',
+      items: [
+        {
+          key: 'RG1.R1',
+          role: 'userInput',
+        },
+        {
+          key: 'RG1.R2',
+          role: 'responseOption',
+          content: [
+            {
+              code: 'en',
+              parts: [
+                {
+                  str: 'I don’t know/can’t remember'
+                },
+              ]
+            },
+            {
+              code: 'de',
+              parts: [
+                {
+                  str: 'XX'
+                },
+              ]
+            },
+          ]
+        },
+        {
+          key: 'RG1.R3',
+          role: 'responseOption',
+          content: [
+            {
+              code: 'en',
+              parts: [
+                {
+                  str: 'Not applicable (e.g. don’t have a fixed workplace)'
+                },
+              ]
+            },
+            {
+              code: 'de',
+              parts: [
+                {
+                  str: 'XX'
+                },
+              ]
+            },
+          ]
+        },
+      ]
+    },
+  ]
+}
+
+const Q4c: SurveySingleItem = {
+  key: 'QG4.Q4c',
+  condition: {
+    name: 'or',
+    data: [
+      {
+        dtype: 'exp',
+        exp: {
+          name: 'isDefined',
+          data: [
+            {
+              dtype: 'exp',
+              exp: {
+                name: 'getResponseItem',
+                data: [
+                  {
+                    str: 'QG4.Q4'
+                  },
+                  {
+                    str: 'RG1.R1'
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      },
+      {
+        dtype: 'exp',
+        exp: {
+          name: 'isDefined',
+          data: [
+            {
+              dtype: 'exp',
+              exp: {
+                name: 'getResponseItem',
+                data: [
+                  {
+                    str: 'QG4.Q4'
+                  },
+                  {
+                    str: 'RG1.R2'
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      }
+    ]
+  },
+  version: 1,
+  validations: [],
+  type: "basic.input.single-choice",
+  components: [
+    {
+      role: 'title',
+      content: [
+        {
+          code: 'en',
+          parts: [
+            {
+              str: 'Which of the following descriptions most closely matches with your main occupation?'
+            },
+          ]
+        },
+        {
+          code: 'de',
+          parts: [
+            {
+              str: 'XX'
+            },
+          ]
+        },
+      ]
+    },
+    {
+      key: 'RG1',
+      role: 'responseGroup',
+      items: [
+        {
+          key: 'RG1.R1',
+          role: 'responseOption',
+          content: [
+            {
+              code: 'en',
+              parts: [
+                {
+                  str: 'Professional (e.g. manager, doctor, teacher, nurse, engineer)'
+                },
+              ]
+            },
+            {
+              code: 'de',
+              parts: [
+                {
+                  str: 'XX'
+                },
+              ]
+            },
+          ]
+        },
+        {
+          key: 'RG1.R2',
+          role: 'userInput',
+          content: [
+            {
+              code: 'en',
+              parts: [
+                {
+                  str: 'Other'
+                },
+              ]
+            },
+            {
+              code: 'de',
+              parts: [
+                {
+                  str: 'XX'
+                },
+              ]
+            },
+          ]
+        },
+      ]
+    },
+  ]
+}
+
+export const QG0: SurveyGroupItem = {
+  key: 'QG0',
   version: 1,
   items: [
     {
-      key: 'QG_4',
+      key: 'QG4',
       version: 1,
       items: [
-        {
-          key: 'QG_4_Q_4',
-          version: 1,
-          validations: [],
-          type: "basic.input.single-choice",
-          components: [
-            {
-              role: 'title', 
-              content: [
-                { code: 'en', parts: ['What is your main activity?'] },
-                { code: 'de', parts: [''] }
-              ]
-            },
-            {
-              key: 'RG_1',
-              role: 'responseGroup', 
-              items: [
-                {
-                  key: 'RG_1_R_1',
-                  role: 'responseOption',
-                  content: [
-                    {
-                      code: 'en',
-                      parts: ['Paid employment, full-time']
-                    },
-                    {
-                      code: 'de',
-                      parts: ['']
-                    }
-                  ]
-                },
-                {
-                  key: 'RG_1_R_2',
-                  role: 'responseOption',
-                  content: [
-                    {
-                      code: 'en',
-                      parts: ['Paid employment, part-time']
-                    },
-                    {
-                      code: 'de',
-                      parts: ['']
-                    }
-                  ]
-                },
-              ]
-            },
-          ]
-        },
-        {
-          key: 'QG_4_Q_4b',
-          condition: {
-            name: 'or',
-            data: [
-              {
-                dtype: "test",
-                exp: 
-                {
-                  name: 'gotYes',
-                  data: [
-                    {
-                      dtype: 'test',
-                      str: 'QG_4_Q_4_RG_1_R_1',
-                    }
-                  ]
-                },
-              },
-              {
-                dtype: "test",
-                exp: 
-                {
-                  name: 'gotYes',
-                  data: [
-                    {
-                      dtype: 'test',
-                      str: 'QG_4_Q_4_RG_1_R_2',
-                    }
-                  ]
-                },
-              },
-            ]
-          },
-          version: 1,
-          validations: [],
-          type: "basic.input.single-choice",
-          components: [
-            {
-              role: 'title', 
-              content: [
-                { code: 'en', parts: ['What is the first part of your school/college/workplace postal code (where you spend the majority of your working/studying time)?'] },
-                { code: 'de', parts: [''] }
-              ]
-            },
-            {
-              key: 'RG_1',
-              role: 'responseGroup', 
-              items: [
-                {
-                  key: 'RG_1_R_1',
-                  role: 'responseInput',
-                  content: [
-                    {
-                      code: 'en',
-                      parts: ['XXXX']
-                    },
-                    {
-                      code: 'de',
-                      parts: ['XXXX']
-                    }
-                  ]
-                },
-                {
-                  key: 'RG_1_R_2',
-                  role: 'responseOption',
-                  content: [
-                    {
-                      code: 'en',
-                      parts: ['I don’t know/can’t remember']
-                    },
-                    {
-                      code: 'de',
-                      parts: ['']
-                    }
-                  ]
-                },
-                {
-                  key: 'RG_1_R_3',
-                  role: 'responseOption',
-                  content: [
-                    {
-                      code: 'en',
-                      parts: ['Not applicable (e.g. don’t have a fixed workplace)']
-                    },
-                    {
-                      code: 'de',
-                      parts: ['']
-                    }
-                  ]
-                },
-              ]
-            },
-          ]
-        },
-        {
-          key: 'QG_4_Q_4c',
-          condition: {
-            name: 'or',
-            data: [
-              {
-                dtype: "test",
-                exp: 
-                {
-                  name: 'gotYes',
-                  data: [
-                    {
-                      dtype: 'test',
-                      str: 'QG_4_Q_4_RG_1_R_1',
-                    }
-                  ]
-                },
-              },
-              {
-                dtype: "test",
-                exp: 
-                {
-                  name: 'gotYes',
-                  data: [
-                    {
-                      dtype: 'test',
-                      str: 'QG_4_Q_4_RG_1_R_2',
-                    }
-                  ]
-                },
-              },
-            ]
-          },
-          version: 1,
-          validations: [],
-          type: "basic.input.single-choice",
-          components: [
-            {
-              role: 'title', 
-              content: [
-                { code: 'en', parts: ['Which of the following descriptions most closely matches with your main occupation?'] },
-                { code: 'de', parts: [''] }
-              ]
-            },
-            {
-              key: 'RG_1',
-              role: 'responseGroup', 
-              items: [
-                {
-                  key: 'RG_1_R_1',
-                  role: 'responseOption',
-                  content: [
-                    {
-                      code: 'en',
-                      parts: ['Professional (e.g. manager, doctor, teacher, nurse, engineer)']
-                    },
-                    {
-                      code: 'de',
-                      parts: ['']
-                    }
-                  ]
-                },
-                {
-                  key: 'RG_1_R_2',
-                  role: 'responseOption',
-                  content: [
-                    {
-                      code: 'en',
-                      parts: ['Other']
-                    },
-                    {
-                      code: 'de',
-                      parts: ['']
-                    }
-                  ]
-                },
-                {
-                  key: 'RG_1_R_3',
-                  role: 'responseInput',
-                  displayCondition: {
-                    name: 'checked',
-                    data: [
-                      {
-                        dtype: 'test',
-                        str: 'QG_4_Q_4C_RG_1_R_2',
-                      },
-                    ]
-                  },
-                  content: [
-                    {
-                      code: 'en',
-                      parts: ['XXXX']
-                    },
-                    {
-                      code: 'de',
-                      parts: ['XXXX']
-                    }
-                  ]
-                },
-              ]
-            },
-          ]
-        },
+        Q4,
+        Q4b,
+        Q4c,
       ],
     }
   ],
 }
+
