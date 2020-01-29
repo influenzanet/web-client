65c002433e091a71d5006c354bd242edb53be671
petterol <o_petter14@cs.uni-kl.de>
2020-01-29 12:10:04 +0100

    implemented Q5 as validation example with warning and Q5b as disabled example

--- src/test-surveys/test-survey-1.ts
+++ src/test-surveys/test-survey-1.ts
@@ -601,19 +601,123 @@ const Q5: SurveySingleItem = {
       key: 'V1',
       type: 'soft',
       rule: {
-        name: 'isDefined',
+        name: 'and',
         data: [
           {
             dtype: 'exp',
             exp: {
-              name: 'getResponseItem',
+              name: 'isDefined',
               data: [
                 {
-                  str: 'QG0.QG5.Q5'
+                  dtype: 'exp',
+                  exp: {
+                    name: 'getResponseItem',
+                    data: [
+                      {
+                        str: 'QG0.QG5.Q5'
+                      },
+                      {
+                        str: 'RG1.R5'
+                      }
+                    ]
+                  }
+                }
+              ]
+            }
+          },
+          {
+            dtype: 'exp',
+            exp: {
+              name: 'or',
+              data: [
+                {
+                  dtype: 'exp',
+                  exp: {
+                    name: 'isDefined',
+                    data: [
+                      {
+                        dtype: 'exp',
+                        exp: {
+                          name: 'getResponseItem',
+                          data: [
+                            {
+                              str: 'QG0.QG5.Q5'
+                            },
+                            {
+                              str: 'RG1.R1'
+                            }
+                          ]
+                        }
+                      }
+                    ]
+                  }
                 },
                 {
-                  str: 'RG1.R5'
-                }
+                  dtype: 'exp',
+                  exp: {
+                    name: 'isDefined',
+                    data: [
+                      {
+                        dtype: 'exp',
+                        exp: {
+                          name: 'getResponseItem',
+                          data: [
+                            {
+                              str: 'QG0.QG5.Q5'
+                            },
+                            {
+                              str: 'RG1.R2'
+                            }
+                          ]
+                        }
+                      }
+                    ]
+                  }
+                },
+                {
+                  dtype: 'exp',
+                  exp: {
+                    name: 'isDefined',
+                    data: [
+                      {
+                        dtype: 'exp',
+                        exp: {
+                          name: 'getResponseItem',
+                          data: [
+                            {
+                              str: 'QG0.QG5.Q5'
+                            },
+                            {
+                              str: 'RG1.R3'
+                            }
+                          ]
+                        }
+                      }
+                    ]
+                  }
+                },
+                {
+                  dtype: 'exp',
+                  exp: {
+                    name: 'isDefined',
+                    data: [
+                      {
+                        dtype: 'exp',
+                        exp: {
+                          name: 'getResponseItem',
+                          data: [
+                            {
+                              str: 'QG0.QG5.Q5'
+                            },
+                            {
+                              str: 'RG1.R4'
+                            }
+                          ]
+                        }
+                      }
+                    ]
+                  }
+                },
               ]
             }
           }
@@ -660,6 +764,99 @@ const Q5: SurveySingleItem = {
           parts: [
             {
               str: 'Wählen Sie alle entsprechenden Optionen aus, falls diese zutreffen'
+            },
+          ]
+        },
+      ]
+    },
+    {
+      key: 'IG1',
+      role: 'helpGroup',
+      order: {
+        name: 'sequential'
+      },
+      items: [
+        {
+          role: 'title',
+          content: [
+            {
+              code: 'en',
+              parts: [
+                {
+                  str: 'Why are we asking this?'
+                },
+              ]
+            },
+            {
+              code: 'de',
+              parts: [
+                {
+                  str: 'Warum fragen wir das?'
+                },
+              ]
+            },
+          ],
+        },
+        {
+          role: 'description',
+          content: [
+            {
+              code: 'en',
+              parts: [
+                {
+                  str: 'To find out whether you are likely to be exposed to more flu than the average person (e.g. work with children, or patients)'
+                },
+              ]
+            },
+            {
+              code: 'de',
+              parts: [
+                {
+                  str: 'Um herauszufinden, ob Sie voraussichtlich stärker der Grippe ausgesetzt sind als der Durchschnittsbürger (z.B. bei der Arbeit mit Kindern oder Patienten)'
+                },
+              ]
+            },
+          ]
+        },
+        {
+          role: 'title',
+          content: [
+            {
+              code: 'en',
+              parts: [
+                {
+                  str: 'How should I answer it?'
+                },
+              ]
+            },
+            {
+              code: 'de',
+              parts: [
+                {
+                  str: 'Wie soll ich darauf antworten?'
+                },
+              ]
+            },
+          ],
+        },
+        {
+          role: 'description',
+          content: [
+            {
+              code: 'en',
+              parts: [
+                {
+                  str: 'Groups of people could include any setting where you come into contact with large numbers of people at once, e.g. a teacher who may contact many children in a day'
+                },
+              ]
+            },
+            {
+              code: 'de',
+              parts: [
+                {
+                  str: 'Personengruppen können jede Situation umfassen, in der Sie mit einer großen Anzahl von Menschen gleichzeitig in Kontakt kommen, z.B. ein Lehrer, der an einem Tag mit vielen Kindern in Kontakt kommen kann'
+                },
+              ]
             },
           ]
         },
@@ -819,6 +1016,345 @@ const Q5: SurveySingleItem = {
   ]
 }
 
+const Q5b: SurveySingleItem = {
+  key: 'QG0.QG5.Q5b',
+  version: 1,
+  validations: [],
+  type: "basic.input.multiple-choice",
+  components: [
+    {
+      role: 'title',
+      content: [
+        {
+          code: 'en',
+          parts: [
+            {
+              str: 'Except people you meet on public contact, do you have contact with any of the following during the course of a typical day?'
+            },
+          ]
+        },
+        {
+          code: 'de',
+          parts: [
+            {
+              str: 'Abgesehen von Menschen, denen Sie in der Öffentlichkeit begegnen, haben Sie im Laufe eines typischen Tages Kontakt mit einer der folgenden Personengruppen?'
+            },
+          ]
+        },
+      ],
+    },
+    {
+      role: 'description',
+      content: [
+        {
+          code: 'en',
+          parts: [
+            {
+              str: 'Select all options that apply, if any'
+            },
+          ]
+        },
+        {
+          code: 'de',
+          parts: [
+            {
+              str: 'Wählen Sie alle entsprechenden Optionen aus, falls diese zutreffen'
+            },
+          ]
+        },
+      ]
+    },
+    {
+      key: 'IG1',
+      role: 'helpGroup',
+      order: {
+        name: 'sequential'
+      },
+      items: [
+        {
+          role: 'title',
+          content: [
+            {
+              code: 'en',
+              parts: [
+                {
+                  str: 'Why are we asking this?'
+                },
+              ]
+            },
+            {
+              code: 'de',
+              parts: [
+                {
+                  str: 'Warum fragen wir das?'
+                },
+              ]
+            },
+          ],
+        },
+        {
+          role: 'description',
+          content: [
+            {
+              code: 'en',
+              parts: [
+                {
+                  str: 'To find out whether you are likely to be exposed to more flu than the average person (e.g. work with children, or patients)'
+                },
+              ]
+            },
+            {
+              code: 'de',
+              parts: [
+                {
+                  str: 'Um herauszufinden, ob Sie voraussichtlich stärker der Grippe ausgesetzt sind als der Durchschnittsbürger (z.B. bei der Arbeit mit Kindern oder Patienten)'
+                },
+              ]
+            },
+          ]
+        },
+        {
+          role: 'title',
+          content: [
+            {
+              code: 'en',
+              parts: [
+                {
+                  str: 'How should I answer it?'
+                },
+              ]
+            },
+            {
+              code: 'de',
+              parts: [
+                {
+                  str: 'Wie soll ich darauf antworten?'
+                },
+              ]
+            },
+          ],
+        },
+        {
+          role: 'description',
+          content: [
+            {
+              code: 'en',
+              parts: [
+                {
+                  str: 'Groups of people could include any setting where you come into contact with large numbers of people at once, e.g. a teacher who may contact many children in a day'
+                },
+              ]
+            },
+            {
+              code: 'de',
+              parts: [
+                {
+                  str: 'Personengruppen können jede Situation umfassen, in der Sie mit einer großen Anzahl von Menschen gleichzeitig in Kontakt kommen, z.B. ein Lehrer, der an einem Tag mit vielen Kindern in Kontakt kommen kann'
+                },
+              ]
+            },
+          ]
+        },
+      ]
+    },
+    {
+      key: 'RG1',
+      role: 'responseGroup',
+      order: {
+        name: 'sequential'
+      },
+      items: [
+        {
+          key: 'RG1.R1',
+          role: 'responseOption',
+          content: [
+            {
+              code: 'en',
+              parts: [
+                {
+                  str: 'More than 10 children or teenagers over the course of the day'
+                },
+              ]
+            },
+            {
+              code: 'de',
+              parts: [
+                {
+                  str: 'Mehr als 10 Kinder oder Jugendliche im Laufe des Tages'
+                },
+              ]
+            },
+          ],
+          disabled: {
+            name: 'isDefined',
+            data: [
+              {
+                dtype: 'exp',
+                exp: {
+                  name: 'getResponseItem',
+                  data: [
+                    {
+                      str: 'QG0.QG5.Q5b'
+                    },
+                    {
+                      str: 'RG1.R5'
+                    }
+                  ]
+                }
+              }
+            ]
+          },
+        },
+        {
+          key: 'RG1.R2',
+          role: 'responseOption',
+          content: [
+            {
+              code: 'en',
+              parts: [
+                {
+                  str: 'More than 10 people aged over 65 over the course of day'
+                },
+              ]
+            },
+            {
+              code: 'de',
+              parts: [
+                {
+                  str: 'Mehr als 10 Personen im Alter von über 65 Jahren im Laufe des Tages'
+                },
+              ]
+            },
+          ],
+          disabled: {
+            name: 'isDefined',
+            data: [
+              {
+                dtype: 'exp',
+                exp: {
+                  name: 'getResponseItem',
+                  data: [
+                    {
+                      str: 'QG0.QG5.Q5b'
+                    },
+                    {
+                      str: 'RG1.R5'
+                    }
+                  ]
+                }
+              }
+            ]
+          },
+        },
+        {
+          key: 'RG1.R3',
+          role: 'responseOption',
+          content: [
+            {
+              code: 'en',
+              parts: [
+                {
+                  str: 'Patients'
+                },
+              ]
+            },
+            {
+              code: 'de',
+              parts: [
+                {
+                  str: 'Patientinnen und Patienten'
+                },
+              ]
+            },
+          ],
+          disabled: {
+            name: 'isDefined',
+            data: [
+              {
+                dtype: 'exp',
+                exp: {
+                  name: 'getResponseItem',
+                  data: [
+                    {
+                      str: 'QG0.QG5.Q5b'
+                    },
+                    {
+                      str: 'RG1.R5'
+                    }
+                  ]
+                }
+              }
+            ]
+          },
+        },
+        {
+          key: 'RG1.R4',
+          role: 'responseOption',
+          content: [
+            {
+              code: 'en',
+              parts: [
+                {
+                  str: 'Groups of people (more than 10 individuals at any one time)'
+                },
+              ]
+            },
+            {
+              code: 'de',
+              parts: [
+                {
+                  str: 'Personengruppen (mehr als 10 Personen zu einem beliebigen Zeitpunkt)'
+                },
+              ]
+            },
+          ],
+          disabled: {
+            name: 'isDefined',
+            data: [
+              {
+                dtype: 'exp',
+                exp: {
+                  name: 'getResponseItem',
+                  data: [
+                    {
+                      str: 'QG0.QG5.Q5b'
+                    },
+                    {
+                      str: 'RG1.R5'
+                    }
+                  ]
+                }
+              }
+            ]
+          },
+        },
+        {
+          key: 'RG1.R5',
+          role: 'responseOption',
+          content: [
+            {
+              code: 'en',
+              parts: [
+                {
+                  str: 'None of the above'
+                },
+              ]
+            },
+            {
+              code: 'de',
+              parts: [
+                {
+                  str: 'Keine der oben genannten'
+                },
+              ]
+            },
+          ]
+        },
+      ]
+    },
+  ]
+}
+
 export const QG0: SurveyGroupItem = {
   key: 'QG0',
   version: 1,
@@ -837,6 +1373,7 @@ export const QG0: SurveyGroupItem = {
       version: 1,
       items: [
         Q5,
+        Q5b,
       ],
     },
   ],
