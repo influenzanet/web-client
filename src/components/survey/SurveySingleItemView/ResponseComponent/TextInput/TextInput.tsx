39353b83bc2013e6787e272e48fe9f3891f6af49
phev8 <phev8@users.noreply.github.com>
2020-04-03 23:32:25 +0200

    implement simple text input component

--- src/components/survey/SurveySingleItemView/ResponseComponent/TextInput/TextInput.tsx
+++ src/components/survey/SurveySingleItemView/ResponseComponent/TextInput/TextInput.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ItemComponent, ResponseItem } from 'survey-engine/lib/data_types';
+import { TextField } from '@material-ui/core';
+import { getLocaleStringTextByCode } from '../../utils';
 
 interface TextInputProps {
   compDef: ItemComponent;
@@ -9,8 +11,55 @@ interface TextInputProps {
 }
 
 const TextInput: React.FC<TextInputProps> = (props) => {
+  const [response, setResponse] = useState<ResponseItem | undefined>(props.prefill);
+  const [touched, setTouched] = useState(false);
+
+  const [inputValue, setInputValue] = useState<string>(
+    props.prefill && props.prefill.value ? props.prefill.value : ''
+  );
+
+  useEffect(() => {
+    if (touched) {
+      const timer = setTimeout(() => {
+        props.responseChanged(response);
+      }, 200);
+      return () => clearTimeout(timer);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [response]);
+
+
+  const handleInputValueChange = (key: string | undefined) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!key) { return; }
+    setTouched(true);
+
+    const value = (event.target as HTMLInputElement).value;
+    setInputValue(value);
+    setResponse(prev => {
+      if (!prev) {
+        return {
+          key: props.compDef.key ? props.compDef.key : 'no key found',
+          value: inputValue
+        }
+      }
+      return {
+        ...prev,
+        value: inputValue
+      }
+    })
+  };
+
+
   return (
-    <p>todo: TextInput</p>
+    <TextField
+      fullWidth
+      label={getLocaleStringTextByCode(props.compDef.content, props.languageCode)}
+      InputLabelProps={{ shrink: true }}
+      value={inputValue}
+      margin="dense"
+      onChange={handleInputValueChange(props.compDef.key)}
+      disabled={props.compDef.disabled !== undefined}
+    ></TextField>
   );
 };
 
