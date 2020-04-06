2908b3a71b535802e4e748d2ec3db78abdfa2e8b
Kevin Brendel <kBrendel@hotmail.de>
2020-04-06 13:13:16 +0200

    Simplified and improved ParticleText implementation.

--- src/components/common/ParticleText/ParticleText.tsx
+++ src/components/common/ParticleText/ParticleText.tsx
@@ -15,14 +15,14 @@ class ParticleText extends React.Component<ParticleTextProps> {
   private particles: Particle[] = [];
   private mousePosition = new Vector2(-999999, -999999);
 
+  private canvasWidth = 0;
+  private canvasHeight = 0;
+
   public componentDidMount() {
     if (this.canvasRef.current) {
       let canvas = this.canvasRef.current;
       this.canvasContext = canvas.getContext("2d");
-      let canvasWidth = canvas.clientWidth * window.devicePixelRatio;
-      let canvasHeight = canvas.clientHeight * window.devicePixelRatio;
-      canvas.width = canvasWidth;
-      canvas.height = canvasHeight;
+      this.udpdateCanvasDimensions(canvas);
 
       window.addEventListener("resize", this.onResize);
       window.addEventListener("mousemove", this.onMouseMove);
@@ -31,23 +31,23 @@ class ParticleText extends React.Component<ParticleTextProps> {
       window.addEventListener("touchend", this.onTouchEnd);
 
       if (this.canvasContext) {
-        this.canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);
+        this.canvasContext.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
 
         this.canvasContext.font = "bold " + this.props.fontSize + " sans-serif";
         this.canvasContext.textAlign = "center";
         this.canvasContext.fillStyle = this.props.fontFillStyle;
-        this.canvasContext.fillText(this.props.text, canvasWidth / 2, canvasHeight / 2);
+        this.canvasContext.fillText(this.props.text, this.canvasWidth / 2, this.canvasHeight / 2);
 
-        let imageData = this.canvasContext.getImageData(0, 0, canvasWidth, canvasHeight).data;
-        this.canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);
+        let imageData = this.canvasContext.getImageData(0, 0, this.canvasWidth, this.canvasHeight).data;
+        this.canvasContext.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
         this.canvasContext.globalCompositeOperation = "source-over";
 
-        for (var x = 0; x < canvasWidth; x += this.pixelStep) {
-          for (var y = 0; y < canvasHeight; y += this.pixelStep) {
-            let pixelStartIndex = ((x + y * canvasWidth) * 4);
+        for (var x = 0; x < this.canvasWidth; x += this.pixelStep) {
+          for (var y = 0; y < this.canvasHeight; y += this.pixelStep) {
+            let pixelStartIndex = ((x + y * this.canvasWidth) * 4);
             if (imageData[pixelStartIndex + 3] > this.alphaThreshold) {
               let color = `rgb(${imageData[pixelStartIndex]}, ${imageData[pixelStartIndex + 1]}, ${imageData[pixelStartIndex + 2]})`;
-              this.particles.push(new Particle(x, y, color, canvasWidth, canvasHeight, this.canvasContext));
+              this.particles.push(new Particle(new Vector2(x, y), color, this.canvasWidth, this.canvasHeight, this.canvasContext));
             }
           }
         }
@@ -71,14 +71,20 @@ class ParticleText extends React.Component<ParticleTextProps> {
       let canvas = this.canvasContext.canvas;
       let oldWidth = canvas.width;
       let oldHeight = canvas.height;
-      canvas.width = canvas.clientWidth * window.devicePixelRatio;
-      canvas.height = canvas.clientHeight * window.devicePixelRatio;
+      this.udpdateCanvasDimensions(canvas);
 
       let deltaX = (canvas.width - oldWidth) / 2;
       let deltaY = (canvas.height - oldHeight) / 2;
 
-      this.particles.forEach((particle) => particle.teleport(deltaX, deltaY));
+      this.particles.forEach((particle) => particle.teleport(new Vector2(deltaX, deltaY)));
     }
+  }
+
+  private udpdateCanvasDimensions(canvas: HTMLCanvasElement) {
+    this.canvasWidth = canvas.clientWidth * window.devicePixelRatio;
+    this.canvasHeight = canvas.clientHeight * window.devicePixelRatio;
+    canvas.width = this.canvasWidth;
+    canvas.height = this.canvasHeight;
   }
 
   private onMouseMove = (ev: MouseEvent) => {
@@ -139,48 +145,34 @@ class Particle {
 
   private canvasContext: CanvasRenderingContext2D;
 
-  constructor(x: number, y: number, color: string, canvasWidth: number, canvasHeight: number, canvasContext: CanvasRenderingContext2D) {
+  constructor(destination: Vector2, color: string, canvasWidth: number, canvasHeight: number, canvasContext: CanvasRenderingContext2D) {
     this.canvasContext = canvasContext;
     this.position = new Vector2(Math.random() * canvasWidth, Math.random() * canvasHeight);
-    this.destination = new Vector2(x, y);
+    this.destination = destination;
     this.color = color;
   }
 
   public renderFrame(mousePosition: Vector2) {
-    this.acceleration.x = (this.destination.x - this.position.x) * this.accelerationFactor;
-    this.acceleration.y = (this.destination.y - this.position.y) * this.accelerationFactor;
-
-    this.velocity.x += this.acceleration.x;
-    this.velocity.y += this.acceleration.y;
-
-    if (this.velocity.x > this.minimumSpeedThreshold) this.velocity.x *= this.frictionFactor;
-    if (this.velocity.y > this.minimumSpeedThreshold) this.velocity.y *= this.frictionFactor;
-
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
+    this.acceleration = this.destination.subtract(this.position).scale(this.accelerationFactor);
+    this.velocity = this.velocity.add(this.acceleration);
+    if (this.velocity.length > this.minimumSpeedThreshold) this.velocity = this.velocity.scale(this.frictionFactor);
+    this.position = this.position.add(this.velocity);
 
     this.canvasContext.fillStyle = this.color;
     this.canvasContext.beginPath();
     this.canvasContext.arc(this.position.x, this.position.y, this.radius, Math.PI * 2, 0, false);
     this.canvasContext.fill();
 
-    let a = this.position.x - mousePosition.x;
-    let b = this.position.y - mousePosition.y;
-
-    let mouseDistance = Math.sqrt(a * a + b * b);
+    let mouseDistance = this.position.subtract(mousePosition).length;
     if (mouseDistance < this.mouseDistanceThreshold) {
-      this.acceleration.x = (this.position.x - mousePosition.x) * this.mouseAccelerationFactor;
-      this.acceleration.y = (this.position.y - mousePosition.y) * this.mouseAccelerationFactor;
-      this.velocity.x += this.acceleration.x;
-      this.velocity.y += this.acceleration.y;
+      this.acceleration = this.position.subtract(mousePosition).scale(this.mouseAccelerationFactor);
+      this.velocity = this.velocity.add(this.acceleration);
     }
   }
 
-  public teleport(x: number, y: number) {
-    this.position.x += x;
-    this.position.y += y;
-    this.destination.x += x;
-    this.destination.y += y;
+  public teleport(deltaVector: Vector2) {
+    this.position = this.position.add(deltaVector);
+    this.destination = this.destination.add(deltaVector);
   }
 
   private getInitialVelocity(): number {
@@ -195,6 +187,26 @@ class Vector2 {
   constructor(x: number, y: number) {
     this.x = x;
     this.y = y;
+  }
+
+  public get length(): number {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+
+  public add(vector: Vector2): Vector2 {
+    return new Vector2(this.x + vector.x, this.y + vector.y);
+  }
+
+  public subtract(vector: Vector2): Vector2 {
+    return new Vector2(this.x - vector.x, this.y - vector.y);
+  }
+
+  public scale(factor: number): Vector2 {
+    return new Vector2(this.x * factor, this.y * factor);
+  }
+
+  public distanceTo(vector: Vector2): number {
+    return this.subtract(vector).length;
   }
 }
 
