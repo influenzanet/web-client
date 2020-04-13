12d4bfce8f9db7b51e0cc156c8f3df79f03ee11d
phev8 <phev8@users.noreply.github.com>
2020-04-13 15:11:16 +0200

    add error page, start with param parsing

--- src/pages/AppIntegration/SurveyWithAuth/SurveyPage/SurveyPage.tsx
+++ src/pages/AppIntegration/SurveyWithAuth/SurveyPage/SurveyPage.tsx
@@ -1,12 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box } from '@material-ui/core';
 import LanguageSelector from '../LanguageSelector/LanguageSelector';
+import { useLocation, useHistory, useRouteMatch, RouteProps } from 'react-router-dom';
+import { survey } from '../../../../test-surveys/qcov';
 
-const SurveyPage: React.FC = () => {
-  // TODO: extract route params
+// A custom hook that builds on useLocation to parse
+// the query string for you.
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+const SurveyPage: React.FC<RouteProps> = (props) => {
+  const query = useQuery();
+  const history = useHistory();
+  let { path: rootPath } = useRouteMatch();
+
+
+  const locale = query.get('locale');
+  const locationID = query.get('locationID');
+  const surveyKey = query.get('surveyKey');
+  const studyKey = query.get('studyKey');
+  const accessToken = query.get('accessToken');
+
+  const [error, setError] = useState(false);
+
+  console.log(locale)
+  console.log(locationID)
+  console.log(surveyKey)
+  console.log(studyKey)
+  console.log(accessToken)
 
   // TODO: use language from i18n
   // TODO: update i18n language when receiving url parameter
+
+  if (!studyKey || !surveyKey || !accessToken) {
+    if (!error) {
+      setError(true);
+    }
+
+  }
+
+  useEffect(() => {
+    if (error) {
+      let to = rootPath.lastIndexOf('/');
+      to = to === -1 ? rootPath.length : to;
+      const newUrl = rootPath.substring(0, to);
+      history.push(`${newUrl}/error`);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [error]);
+
 
 
   const [selectedLanguage, setSelectedLanguage] = useState('de');
@@ -19,7 +62,7 @@ const SurveyPage: React.FC = () => {
   const languageSelector = (
     <Box display="flex">
       <Box flexGrow={1}></Box>
-      <Box>
+      <Box pr={2}>
         <LanguageSelector
           selected={selectedLanguage}
           availableLanguages={availableLanguages}
--- src/pages/AppIntegration/SurveyWithAuth/SurveyWithAuth.tsx
+++ src/pages/AppIntegration/SurveyWithAuth/SurveyWithAuth.tsx
@@ -4,6 +4,7 @@ import { useRouteMatch, Switch, Route, Redirect } from 'react-router-dom';
 import SurveyPage from './SurveyPage/SurveyPage';
 import SurveyEndPage from './SurveyEndPage/SurveyEndPage';
 import ClosePage from './ClosePage/ClosePage';
+import ErrorPage from './ErrorPage/ErrorPage';
 
 const SurveyWithAuth: React.FC = () => {
   let { path: rootPath } = useRouteMatch();
@@ -15,6 +16,7 @@ const SurveyWithAuth: React.FC = () => {
         <Route path={`${rootPath}/survey`} component={SurveyPage} />
         <Route path={`${rootPath}/survey-end`} component={SurveyEndPage} />
         <Route path={`${rootPath}/close`} component={ClosePage} />
+        <Route path={`${rootPath}/error`} component={ErrorPage} />
         <Redirect to={`${rootPath}/survey`}></Redirect>
       </Switch>
     </React.Fragment>
