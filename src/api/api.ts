8f732c91e5a9a85c9dcf2c9e903697f00a91ed14
phev8 <phev8@users.noreply.github.com>
2020-04-13 16:47:13 +0200

    use local environment value to define api base url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,11 +1,16 @@
 import axios from 'axios';
 import { UserCredentials, TokenResponse } from './models/auth-api';
+import { Survey } from 'survey-engine/lib/data_types';
 
 const api = axios.create({
-  baseURL: `http://localhost:3231`
+  // baseURL: `http://localhost:3231`
+  baseURL: process.env.REACT_APP_API_BASE_URL
 });
 export default api;
 
 // Auth API
 export const signupWithEmailRequest = (creds: UserCredentials) => api.post<TokenResponse>('/v1/auth/signupWithEmail', creds);
 export const loginWithEmailRequest = (creds: UserCredentials) => api.post<TokenResponse>('/v1/auth/loginWithEmail', creds);
+
+
+export const getAssignedSurveyRequest = (payload: UserCredentials) => api.post('/v1/study-system/study/', payload);
