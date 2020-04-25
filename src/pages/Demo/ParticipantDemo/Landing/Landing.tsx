58e4985ecf2bb64c054fae8cf5028955e4c6216c
phev8 <phev8@users.noreply.github.com>
2020-04-25 15:10:24 +0200

    Add landing page content to demo

--- src/pages/Demo/ParticipantDemo/Landing/Landing.tsx
+++ src/pages/Demo/ParticipantDemo/Landing/Landing.tsx
@@ -1,8 +1,54 @@
 import React from 'react';
 
+import logo from './logo.png';
+import { Box, Container, Typography } from '@material-ui/core';
+import RoundedButton from '../../../../components/ui/buttons/RoundedButton';
+import { useLocation, useHistory } from 'react-router-dom';
+
 const Landing: React.FC = () => {
+  const location = useLocation();
+  const history = useHistory();
+
   return (
-    <p>Landing</p>
+    <Container maxWidth="md">
+      <Box textAlign="center" m={6}>
+        <Typography variant="h6" color="secondary">
+          {'Registration Successful'}
+        </Typography>
+      </Box>
+      <Box textAlign="center" m={4}>
+        <img src={logo} alt="Logo" width={200} />
+      </Box>
+      <Box textAlign="center" m={6}>
+        <Typography variant="h5" color="primary">
+          {'Thank you for joining our'}
+        </Typography>
+        <Typography variant="h5" color="primary" style={{ fontWeight: 'bold' }}>
+          {'25342 volunteers,'}
+        </Typography>
+        <Typography variant="h5" color="primary">
+          {'your contribution will count.'}
+        </Typography>
+      </Box>
+      <Box textAlign="center" mt={8}>
+        <Typography variant="h6" color="secondary">
+          {'You have one new survey assigned:'}
+        </Typography>
+        <Box textAlign="center" p={1}>
+          <RoundedButton onClick={
+            () => {
+              alert("In this demo, your answers won't be recorded.")
+              const url = location.pathname.slice(0, location.pathname.lastIndexOf('/') + 1) + 'survey';
+              history.replace(url);
+
+            }
+          }>
+            Take me to the survey
+        </RoundedButton>
+        </Box>
+
+      </Box>
+    </Container>
   );
 };
 
