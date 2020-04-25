ddb534515ea6ad1f72cf3c78dd5a458e350f8061
phev8 <phev8@users.noreply.github.com>
2020-04-25 13:33:00 +0200

    Add list icons

--- src/pages/Demo/MonitoringDemo/AppDrawer/AppDrawer.tsx
+++ src/pages/Demo/MonitoringDemo/AppDrawer/AppDrawer.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { Drawer, Toolbar } from '@material-ui/core';
+import { Drawer, Toolbar, List, ListItem, ListItemText, Divider, Tooltip, ListItemIcon } from '@material-ui/core';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
+import BarChartOutlinedIcon from '@material-ui/icons/BarChartOutlined';
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
 
 interface AppDrawerProps {
 }
@@ -36,10 +39,36 @@ const AppDrawer: React.FC<AppDrawerProps> = (props) => {
     >
       <Toolbar />
       <div className={classes.drawerContainer}>
-        Test
+        <List component="nav" aria-label="main mailbox folders">
+          <ListItem button>
+            <Tooltip title="Select an other study">
+              <ListItemText primary="COVID-19 study" />
+            </Tooltip>
+          </ListItem>
+          <Divider />
+          <ListItem button>
+            <ListItemIcon>
+              <HomeOutlinedIcon />
+            </ListItemIcon>
+            <ListItemText primary="Dashboard" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <BarChartOutlinedIcon />
+            </ListItemIcon>
+            <ListItemText primary="Analytics" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <EditOutlinedIcon />
+            </ListItemIcon>
+            <ListItemText primary="Study Editor" />
+          </ListItem>
+        </List>
+
       </div>
     </Drawer>
-  );
+  )
 };
 
 export default AppDrawer;
