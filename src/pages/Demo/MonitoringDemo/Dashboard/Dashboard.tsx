ddc93699e6cd57029f8e87964165d49887453a2b
phev8 <phev8@users.noreply.github.com>
2020-04-25 18:10:57 +0200

    install vega and add example plot to dashboard

--- src/pages/Demo/MonitoringDemo/Dashboard/Dashboard.tsx
+++ src/pages/Demo/MonitoringDemo/Dashboard/Dashboard.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
+import { VegaLite } from 'react-vega';
 
-const Dashboard: React.FC = () => {
+
+const spec = JSON.stringify({
+  "description": "A simple bar chart with embedded data.",
+  "mark": "bar",
+  data: { name: 'values' },
+  "encoding": {
+    "x": { "field": "a", "type": "ordinal" },
+    "y": { "field": "b", "type": "quantitative" }
+  }
+})
+
+const barData = {
+  "values": [
+    { "a": "A", "b": 20 }, { "a": "B", "b": 34 }, { "a": "C", "b": 55 },
+    { "a": "D", "b": 19 }, { "a": "E", "b": 40 }, { "a": "F", "b": 34 },
+    { "a": "G", "b": 91 }, { "a": "H", "b": 78 }, { "a": "I", "b": 25 }
+  ]
+};
+
+const Dashboard = () => {
   return (
-    <p>Dashboard Dashboard Dashboard Dashboard Dashboard Dashboard</p>
+    <div>
+      <VegaLite spec={JSON.parse(spec)} data={barData} />
+    </div>
 
   );
 };
