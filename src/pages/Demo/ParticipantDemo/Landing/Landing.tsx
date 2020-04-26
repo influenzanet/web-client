67b003318ddd9d9bcffc40be92862e260ed9cbf5
Kevin Brendel <kBrendel@hotmail.de>
2020-04-26 19:51:57 +0200

    Small improvements to layout of Landing page and SurveyEnd page.

--- src/pages/Demo/ParticipantDemo/Landing/Landing.tsx
+++ src/pages/Demo/ParticipantDemo/Landing/Landing.tsx
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 import logo from './logo.png';
 import { Box, Container, Typography } from '@material-ui/core';
 import RoundedButton from '../../../../components/ui/buttons/RoundedButton';
 import { useLocation, useHistory } from 'react-router-dom';
+import FlexGrow from '../../../../components/common/FlexGrow';
 
 const Landing: React.FC = () => {
   const location = useLocation();
   const history = useHistory();
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.style.height = `calc(100vh - ${containerRef.current.offsetTop}px)`;
+    }
+  });
 
   return (
-    <Container maxWidth="md">
-      <Box textAlign="center" m={6}>
-        <Typography variant="h6" color="secondary">
+    <Container ref={containerRef} style={{ display: "flex", flexDirection: "column" }} maxWidth="md">
+      <FlexGrow />
+      <Box textAlign="center" m={4}>
+        <Typography variant="h4" color="secondary">
           {'Registration Successful'}
         </Typography>
       </Box>
-      <Box textAlign="center" m={4}>
+      <FlexGrow />
+      <Box textAlign="center" m={2}>
         <img src={logo} alt="Logo" width={200} />
       </Box>
       <Box textAlign="center" m={6}>
@@ -24,13 +34,14 @@ const Landing: React.FC = () => {
           {'Thank you for joining our'}
         </Typography>
         <Typography variant="h5" color="primary" style={{ fontWeight: 'bold' }}>
-          {'25342 volunteers,'}
+          {'25342 volunteers.'}
         </Typography>
         <Typography variant="h5" color="primary">
-          {'your contribution will count.'}
+          {'Your contribution will count!'}
         </Typography>
       </Box>
-      <Box textAlign="center" mt={8}>
+      <FlexGrow />
+      <Box textAlign="center" m={2}>
         <Typography variant="h6" color="secondary">
           {'You have one new survey assigned:'}
         </Typography>
@@ -48,8 +59,10 @@ const Landing: React.FC = () => {
             Take me to the survey
         </RoundedButton>
         </Box>
-
       </Box>
+      <FlexGrow />
+      <FlexGrow />
+      <FlexGrow />
     </Container>
   );
 };
