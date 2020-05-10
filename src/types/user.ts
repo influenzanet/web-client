84628bc42c9b32d4b493d477b89b197340d21fba
phev8 <phev8@users.noreply.github.com>
2020-05-10 23:53:53 +0200

    add user model to typescript

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,3 +1,22 @@
+export interface User {
+  id: string;
+  account: {
+    type: string;
+    accountId: string;
+    accountConfirmedAt: number;
+    preferredLanguage: string;
+  };
+  roles: string[];
+  timestamps: {
+    createdAt: number;
+    updatedAt: number;
+    lastLogin: number;
+    lastTokenRefresh: number;
+  };
+  profiles: Profile[];
+  contactPreferences: ContactPreferences;
+  contanctInfos: ContactInfo[];
+}
 
 export interface Profile {
   id: string;
@@ -6,3 +25,24 @@ export interface Profile {
   avatarId: string;
   createdAt: number;
 }
+
+export interface ContactPreferences {
+  subscribedToNewsletter: boolean;
+  sendNewsletterTo: string[];
+}
+
+interface ContactInfoBase {
+  id: string;
+  type: string;
+  confirmedAt: number;
+}
+
+export interface EmailContactInfo extends ContactInfoBase {
+  email: string;
+}
+
+export interface PhoneContactInfo extends ContactInfoBase {
+  phone: string;
+}
+
+export type ContactInfo = EmailContactInfo | PhoneContactInfo;
