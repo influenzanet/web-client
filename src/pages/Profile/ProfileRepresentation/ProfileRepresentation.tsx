70507bc90d82a27d148b907d5bbd4edb75e92f79
Kevin Brendel <kBrendel@hotmail.de>
2020-05-16 20:58:14 +0200

    Added Avatars.

--- src/pages/Profile/ProfileRepresentation/ProfileRepresentation.tsx
+++ src/pages/Profile/ProfileRepresentation/ProfileRepresentation.tsx
@@ -15,7 +15,8 @@ const useStyles = makeStyles((theme: Theme) =>
     avatar: {
       width: 120,
       height: 120,
-      fontSize: 42,
+      userSelect: "none",
+      objectFit: "scale-down",
     },
     alias: {
       width: 136,
@@ -49,15 +50,54 @@ const ProfileRepresenation: React.FC<ProfileRepresentationProps> = (props) => {
     }
   });
 
+  let avatarPath = "/static/images/avatar/Unknown_Bunt.png";
+
+  switch (props.profile.avatarId) {
+    case "default_default":
+      avatarPath = "/static/images/avatar/Unknown_Bunt.png";
+      break;
+    case "default_alpaca":
+      avatarPath = "/static/images/avatar/Alpaka_Bunt.png";
+      break;
+    case "default_dolphin":
+      avatarPath = "/static/images/avatar/Delfin_Bunt.png";
+      break;
+    case "default_elephant":
+      avatarPath = "/static/images/avatar/Elefant_Bunt.png";
+      break;
+    case "default_owl":
+      avatarPath = "/static/images/avatar/Eule_Bunt.png";
+      break;
+    case "default_flamingo":
+      avatarPath = "/static/images/avatar/Flamingo_Bunt.png";
+      break;
+    case "default_fox":
+      avatarPath = "/static/images/avatar/Fuchs_Bunt.png";
+      break;
+    case "default_gecko":
+      avatarPath = "/static/images/avatar/Gecko_Bunt.png";
+      break;
+    case "default_panda":
+      avatarPath = "/static/images/avatar/Panda_Bunt.png";
+      break;
+    case "default_penguin":
+      avatarPath = "/static/images/avatar/Pinguin_Bunt.png";
+      break;
+    case "default_spider":
+      avatarPath = "/static/images/avatar/Spinne_Bunt.png";
+      break;
+    case "default_bull":
+      avatarPath = "/static/images/avatar/Stier_Bunt.png";
+      break;
+  }
+
   const onSelected = () => {
     props.onSelected(props.profile);
   }
 
   return (
     <Grid item container className={classes.avatarContainer} direction="column" alignItems="center" onClick={onSelected}>
-      <Avatar className={classes.avatar} style={{ backgroundColor: props.profile.avatarId }}>
-        {initials}
-      </Avatar>
+      <img className={classes.avatar} src={process.env.PUBLIC_URL + avatarPath} />
       <div className={classes.spacer} />
       <Typography variant="subtitle1" className={classes.alias} noWrap={true} >
         {props.profile.alias}
