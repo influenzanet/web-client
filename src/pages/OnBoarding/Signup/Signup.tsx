a82aae8f3b430ed86189c97a832c566c94d0af20
Kevin Brendel <kBrendel@hotmail.de>
2020-05-20 19:45:13 +0200

    WIP adjustments to signup page to get it closer to target featureset.

--- src/pages/OnBoarding/Signup/Signup.tsx
+++ src/pages/OnBoarding/Signup/Signup.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect, Fragment } from 'react';
 import { LinkRef } from '../../../components/common/link';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
@@ -10,113 +10,145 @@ import TextField from '@material-ui/core/TextField';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 import logo from '../../../assets/images/Influenzanet_Logo_RGB.png';
 import Box from '@material-ui/core/Box';
+import RoundedBox from '../../../components/ui/RoundedBox';
+import FlexGrow from '../../../components/common/FlexGrow';
+import { Tooltip } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
-    '@global': {
-        body: {
-            backgroundColor: theme.palette.common.white,
-        },
-    },
-    paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        // backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
+  pageContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  textContainer: {
+    marginTop: theme.spacing(2),
+  },
+  formContainer: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    // backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  errorText: {
+    color: "white",
+  }
 }));
 
 const Signup: React.FC = () => {
-    const classes = useStyles();
+  const classes = useStyles();
+  const containerRef = useRef<HTMLDivElement>(null);
+  const theme = useTheme();
 
-    return (
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <div className={classes.paper}>
-                <Box className={classes.avatar} height="80px">
-                    <img src={logo} alt="logo" height="100%"/>
-                </Box>
-                <Typography component="h1" variant="h5">
-                    Sign Up
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.style.minHeight = `calc(100vh - ${containerRef.current.offsetTop}px)`;
+    }
+  }, []);
+
+  return (
+    <Container ref={containerRef} className={classes.pageContainer} maxWidth="xs" >
+      <FlexGrow />
+      <Box className={classes.avatar} height="80px">
+        <img src={logo} alt="logo" height="100%" />
+      </Box>
+      <Typography variant="h3" color="primary">
+        Sign Up
                 </Typography>
-                <form className={classes.form} noValidate>
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="confirmPassword"
-                        label="Confirm Password"
-                        type="password"
-                        id="confirmPassword"
-                        autoComplete="current-password"
-                    />
-                    <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Do you accept the terms and conditions. (Ref. here)"
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        component={LinkRef} to="/home"
-                        className={classes.submit}
-                    >
-                        Sign Up
+      <RoundedBox classNames={[classes.textContainer]}>
+        <Typography variant="body1" color="primary">
+          Register now to support the global fight against the novel coronavirus and influenza-like-illnesses!
+        </Typography>
+      </RoundedBox>
+      <RoundedBox classNames={[classes.formContainer]}>
+        <form className={classes.form} noValidate>
+          <Tooltip title="We need this so we can identify you.">
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+          </Tooltip>
+          <Tooltip title="Don't make it too long!">
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+          </Tooltip>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="confirmPassword"
+            label="Confirm Password"
+            type="password"
+            id="confirmPassword"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label={
+              <Fragment><span>I have read and accept the </span> <Link variant="body1" component={LinkRef} to="/privacy">privacy statement</Link><span>.</span></Fragment>
+            }
+          />
+          <Tooltip title="If you want to, we will remind you via E-Mail when new surveys are available in your subscribed studies. We will also send you updates about the project every few weeks.">
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="I want to receive the newsletter and survey reminders."
+            />
+          </Tooltip>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            component={LinkRef} to="/home"
+            className={classes.submit}
+          >
+            Sign Up
                     </Button>
-                    <Grid container>
-                        <Grid item xs>
-                            <Link href="#" variant="body2">
-                                Forgot password?
-                            </Link>
-                        </Grid>
-                        <Grid item>
-                            <Link variant="body2" component={LinkRef} to="/start/login">
-                                {"Already have an account? Sign in"}
-                            </Link>
-                        </Grid>
-                    </Grid>
-                </form>
-            </div>
-        </Container>
-    );
+          <Link variant="body2" component={LinkRef} to="/start/login" align="center">
+            {"Already have an account? Sign in."}
+          </Link>
+        </form>
+      </RoundedBox>
+      <RoundedBox color={theme.palette.error.main}>
+        <Typography variant="body1" color="inherit" className={classes.errorText}>
+          Password too long.
+        </Typography>
+      </RoundedBox>
+      <FlexGrow />
+    </Container>
+  );
 }
 
 export default Signup;
